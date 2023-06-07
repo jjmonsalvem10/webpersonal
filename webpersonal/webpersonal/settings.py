d48cce97faade3f97b7dddc1b99bb2f77849c3b1
jjmonsalvem10 <monsalve1994@gmail.com>
2023-06-07 00:43:07 -0500

    Revert "Prueba PostgreSQL"
    This reverts commit efccd15a469b66b7f4b465d9df65a4ca7aa8f451.

--- webpersonal/webpersonal/settings.py
+++ webpersonal/webpersonal/settings.py
@@ -76,21 +76,10 @@ WSGI_APPLICATION = 'webpersonal.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-"""DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}"""
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DATABASE_NAME'),
-        'USER': os.environ.get('DATABASE_USER'),
-        'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
-        'HOST': os.environ.get('DATABASE_HOST'),
-        'PORT': os.environ.get('DATABASE_PORT'),
     }
 }
 
